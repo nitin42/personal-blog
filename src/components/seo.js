96fb48ee5a20fc60c546a7594ac29b9117354642
Nitin Tulswani <nitintulswani@nitins-mbp.mynet>
2020-03-29 15:12:08 +0100

    Add favicon

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,7 +2,7 @@ import React from "react"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-// import favicon from "../images/favicon.ico"
+import favicon from "../images/favicon.ico"
 
 function SEO() {
   const { site } = useStaticQuery(
@@ -28,14 +28,7 @@ function SEO() {
     >
       <meta charSet="utf-8" />
       <meta name="description" content={site.siteMetadata.description} />
-      <link
-        rel="stylesheet"
-        href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
-        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
-        crossorigin="anonymous"
-      />
-      {/* <link rel="icon" href={favicon} /> */}
-      {/* <link rel="manifest" href="./icons/site.webmanifest" /> */}
+      <link rel="icon" href={favicon} />
     </Helmet>
   )
 }
