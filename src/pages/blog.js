235b098fb844c36aec1153efecb0e301d0856912
Nitin Tulswani <nitintulswani@nitins-mbp.mynet>
2020-04-13 16:16:51 +0100

    Some copy changes in first blog post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -11,13 +11,13 @@ export default () => {
     <Layout>
       <Container
         centerAlign
-        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-5"
+        className="pt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-5"
       >
         <GatsbyLink to="/blog/moire" className="mx-auto">
           <Card
             imageUrl={moireImage}
             heading="Moire pattern"
-            description="An interactive visualisation explaining the Moire pattern."
+            description="What is Moiré and how it can be used to create custom patterns."
           />
         </GatsbyLink>
       </Container>
